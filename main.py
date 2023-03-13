9b0c82cf9b9f510520186139c606b19d59996001
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-13 11:11:35 -0400

    fix bug

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ app = Flask('app')
 def home():
     return 'Hi from flask'
 
-@app.route('/<path:path>')
+@app.route('/<path:path>',methods = ['POST', 'GET'])
 def fetch(path):
-    request.cookies.get('url')
-    data = '''<!DOCTYPE html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><style>html, body {margin: 0;padding: 0;height: 100%;overflow: hidden;}iframe {display: block;width: 100%;height: 100%;border: none;}img {position: absolute;top: 0;right: 0;}</style><script src="https://unpkg.com/@ungap/custom-elements-builtin"></script><script src="https://franck403.github.io/x-frame-bypass/iframe.hitsab.js" type="module"></script></head><body><iframe is="x-frame-bypass" src="https://''' + path + '''"></iframe></body></html>'''
+    if request.method == 'GET':
+        url = request.cookies.get('url')
+        requests.get(url)
+        data = '''<!DOCTYPE html><html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><style>html, body {margin: 0;padding: 0;height: 100%;overflow: hidden;}iframe {display: block;width: 100%;height: 100%;border: none;}img {position: absolute;top: 0;right: 0;}</style><script src="https://unpkg.com/@ungap/custom-elements-builtin"></script><script src="https://franck403.github.io/x-frame-bypass/iframe.hitsab.js" type="module"></script></head><body><iframe is="x-frame-bypass" src="https://''' + path + '''"></iframe></body></html>'''
+    else:
+        data = "not supported for now"
     return data
 
-app.run(host='0.0.0.0', port=8080)
+app.run()
